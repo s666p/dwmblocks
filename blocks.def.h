e264be3977ac842e27b933b462b15c6d1276b023
Punk <s666punk@gmail.com>
2021-06-01 00:30:16 +0300

    add src files

---
+++ blocks.def.h
@@ -0,0 +1,18 @@
+/* macro for conveniently specifying path below */
+#define PATH(name)                      "<path to the folder containing block scripts>/"name
+
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/	 	/*Update Interval*/	/*Update Signal*/
+        {"",            PATH("calendar.sh"),	30,		        1},
+
+        {"",            PATH("battery.sh"),	30,		        2},
+
+        {"",            PATH("cpu_temp.sh"),	1,		        3},
+	
+        {"",            PATH("volume.sh"),	1,    	                4},
+
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim = '|';
